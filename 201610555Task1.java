222b5b4c1adcdadb846dece17e0333c1dcb97c00
raafat hamdan <raafat_hamdan@hotmail.com>
2021-11-01 14:13:33 +0200

    added a default value

--- 201610555Task1.java
+++ 201610555Task1.java
@@ -1,13 +1,19 @@
 public class Hello 
 {
 
-public static void main(String args[])
+public static void main(String argv[])
 {
 
-System.out.println("Hello , World")
+String name = " World";
 
-}
+if (argv.length !=0)
+	
+	{
+	name = argv[0];	
+	}
 
+
+System.out.println ("Hello ," +name+"!");
 
 
 }
